656fcf044ddf978600300821f0311269e64a87f5
Emily Gaska <emilygaska@gmail.com>
2020-04-22 21:37:19 -0500

    Corrected if statements to used .push() to add characters to the passwordCharacters array

--- Develop/script.js
+++ Develop/script.js
@@ -2,40 +2,73 @@
 var lowercaseAlphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
 var uppercaseAlphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
 var numberCharacters = "1 2 3 4 5 6 7 8 9".split(" ");
-var specialCharacters = "~ ! @ # $ % ^ & * ( ) _ - + = [ ] { } | ; : '  , . / ?".split("");
+var specialCharacters = "~ ! @ # $ % ^ & * ( ) _ - + = [ ] { } | ; : / ?".split(" ");
 var passwordCharacters = [];
 var generatedPasswordNumbers = [];
+var passwordFinal = [];
 
 
 // Prompt Questions
-
 var passwordLength = prompt("How many characters would you like the password to be? (Please enter an integer)");
 var lower = confirm("Do you want lowercase characters?");
 var upper = confirm("Do you want uppercase characters?");
 var numbers = confirm("Do you want numbers?");
 var special = confirm("Do you want special characters?");
 
-//If loops to add requested charactertypes
+//If statements to add requested character types
 if (lower === true) {
-  passwordCharacters = passwordCharacters + lowercaseAlphabet;
-}
-if (upper === true) {
-  passwordCharacters = passwordCharacters + uppercaseAlphabet;
-}
-if (numbers === true) {
-  passwordCharacters = passwordCharacters + numberCharacters;
-}
-if (special === true) {
-  passwordCharacters = passwordCharacters + specialCharacters;
+  for (let i = 0; i < lowercaseAlphabet.length - 1; i++) {
+    passwordCharacters.push(lowercaseAlphabet[i]);
+  }
+  //   //Test Console Display
+  //   console.log("Lowercase: " + passwordCharacters);
+  
+  if (upper === true) {
+    for (let i = 0; i < uppercaseAlphabet.length - 1; i++) {
+      passwordCharacters.push(uppercaseAlphabet[i]);
+    }
+  }
+      // //Test Console Display
+      // console.log("Uppercase: " + passwordCharacters);
+
+  if (numbers === true) {
+    for (let i = 0; i < numberCharacters.length - 1; i++) {
+      passwordCharacters.push(numberCharacters[i]);
+    }
+  }
+
+      // //Test Console Display
+      // console.log("Numbers: " + passwordCharacters);
+
+  if (special === true) {
+    for (let i = 0; i < specialCharacters.length - 1; i++) {
+      passwordCharacters.push(specialCharacters[i]);
+    }
+  }
+      //Test Console Display
+      // console.log("Special: " + passwordCharacters);
 }
 
- for (let i = 1; i < passwordLength; i++){
+// Loop to generate the requested amount of random slots to be entered into the array passwordCharacters.
+for (let i = 1; i < passwordLength; i++) {
   var characterSlot = passwordCharacters.length * Math.random();
   characterSlot = parseInt(characterSlot);
   generatedPasswordNumbers.push(characterSlot);
 
-  console.log(generatedPasswordNumbers);
- }
+  // //Test Display
+  // console.log("Generated Passwords Numbers: " + generatedPasswordNumbers);
+}
+console.log("Password Characters: " + passwordCharacters[1]);
+
+//Loop to convert the generatedPasswordNumbers into useable characters.
+for (let i = 0; i < passwordLength - 1; i++) {
+
+  passwordFinal.push(passwordCharacters[generatedPasswordNumbers[i]])
+}
+
+//Test Display
+console.log(passwordFinal);
+
 
 
 
